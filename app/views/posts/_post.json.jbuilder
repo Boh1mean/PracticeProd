19974fb4c742a154f08759d0ca1e0ba454818e55
Danil Knyazkin <danya.knyazkin@gmail.com>
2024-07-04 00:34:20 +0300

    lowVerstka

--- app/views/posts/_post.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! post, :id, :title, :description, :keywords, :created_at, :updated_at
-json.url post_url(post, format: :json)
--- db/migrate/20240703143035_create_posts.rb
+++ db/migrate/20240703143035_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :image
       t.string :description
-      t.string :keywords
+      t.integer :user_id
 
       t.timestamps
     end
